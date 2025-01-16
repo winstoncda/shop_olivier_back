d96f2350f99763baa654ca9be52ed29c452cd23d
winstoncda <olivier.pro.cda@gmail.com>
2025-01-16 16:05:43 +0100

    maj 16 janvier

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const connection = require("../database/configDB");
 
+//  création de la route pour récupérer toutes les données de la table products
+// url : http://localhost:5000
 router.get("/", (req, res) => {
   const sql = "SELECT * FROM products";
   connection.query(sql, (err, result) => {
