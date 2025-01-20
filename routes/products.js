2afb490d98ca014080419c84c86072fa04613356
winstoncda <olivier.pro.cda@gmail.com>
2025-01-20 10:59:25 +0100

    ajout d'un article en base de données

--- routes/products.js
+++ routes/products.js
@@ -11,4 +11,15 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/addToCart", (req, res) => {
+  console.log(req.body);
+  const { id, title, price } = req.body;
+  const insertSql =
+    "INSERT INTO cart (title, price, idProduct) VALUES (?, ?, ?)";
+  connection.query(insertSql, [title, price, id], (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ message: "OK" });
+  });
+});
+
 module.exports = router;
